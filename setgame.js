68d1e31a5cefaa2184c9e89a3db478173eee99af
nafTusTypie <37276870+nafTusTypie@users.noreply.github.com>
2019-02-06 17:28:20 +0100

    Create setgame.js

---
+++ setgame.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json');
+
+client.on('ready', () => {
+	console.log('Zalogowano jako ${client.user.nametag}');
+  console.log('Zrobione przez : nafTus');
+});
+
+client.on("message", async message => {
+	if(message.author.bot) return;
+	if(message.content.indexOf(prefix) !== 0) return;
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+	
+	if(command == "setcustomgame") {
+		if(message.author.id != config.ownerid) {
+			return;
+		}
+		let game = args.slice(0).join(' ');
+		if(!game) game = "";
+		await client.user.setActivity(game);
+		console.log(`Gra : ${game}`);
+	}
+	if(command == "setstatus") {
+		if(message.author.id != config.ownerid) {
+			return;
+		}
+		let status = args.slice(0).join(' ');
+		if(!status) status = "online";
+		await client.user.setStatus(status);
+		console.log(`Status : ${status}`);
+	}
+});
+
+client.login(config.token);
